780451e73b516335b2235ac29415dbfeb2078270
Isaac Kuehnle-Nelson <ikethepike@outlook.com>
2019-02-06 13:30:35 +0100

    Expanded resource controllers and defined primary api methods

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace AlleyCat\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //
+}
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,21 @@ use Illuminate\Http\Request;
 |
 */
 
+/* Unprotected user routes */
+Route::group(['prefix' => 'user'], function () {
+    Route::post('login', 'UserController@login');
+    Route::post('register', 'UserController@register');
+    Route::post('exists', 'UserController@exists');
+});
+
+/* Protected Resource Routes */
+Route::group(['middleware' => ['auth:api']], function () {
+    Route::resource('race', 'RaceController');
+    Route::resource('competitor', 'CompetitorController');
+    Route::resource('stat', 'StatController');
+    Route::resource('checkpoint', 'CheckpointController');
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
